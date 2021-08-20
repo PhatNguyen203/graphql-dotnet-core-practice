0655da09b79c4e03323d0932eb292bda7163bb0f
Lee0ne203 <Lee0ne203@gmail.com>
2021-08-19 23:47:29 -0400

    Start to create a complex type

--- GraphQLDotNetCore/GraphQLDotNetCore/GraphQL/GraphQLTypes/AccountType.cs
+++ GraphQLDotNetCore/GraphQLDotNetCore/GraphQL/GraphQLTypes/AccountType.cs
@@ -1,4 +1,5 @@
 ﻿using GraphQL.Types;
+using GraphQLDotNetCore.Entities;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -6,7 +7,13 @@ using System.Threading.Tasks;
 
 namespace GraphQLDotNetCore.GraphQL.GraphQLTypes
 {
-	public class AccountType : ObjectGraphType
+	public class AccountType : ObjectGraphType<Account>
 	{
+		public AccountType()
+		{
+			Field(x => x.Id, type: typeof(IdGraphType)).Description("Id property from the account object.");
+			Field(x => x.Description).Description("Description property from the account object.");
+			Field(x => x.OwnerId, type: typeof(IdGraphType)).Description("OwnerId property from the account object.");
+		}
 	}
 }
